Fixes some bugs in regressor selection and masking

cascade/TSO/TSO.py
@@ -673,8 +673,9 @@ class TSOSuite:
             # number of good measurements at a given time
             number_of_bad_data = np.ma.count(sorted_matrix, axis=0)
             idx_bad = np.argsort(number_of_bad_data)
-            nclip = np.rint(dim[1]*clip_pctl_time).astype(int)
+            nclip = np.rint(dim[-1]*clip_pctl_time).astype(int)
             idx_clip_time = idx_bad[:nclip]
+
             # clip the worst regressors
             # number of good measurements for regressor.
             number_of_bad_data = np.ma.count(sorted_matrix, axis=1)
@@ -694,12 +695,18 @@ class TSOSuite:
             cleaned_mask = np.ma.make_mask_none(cleaned_matrix.shape)
             cleaned_mask[:, idx_clip_time] = True
             cleaned_mask[idx_clip_regressor, :] = True
-            design_matrix = np.ma.array(cleaned_matrix*data_unit,
-                                        mask=cleaned_mask)
+
+            idx_reverse = np.argsort(idx_sort)
+            design_matrix = \
+                np.ma.array(cleaned_matrix[idx_reverse, :],
+                            mask=cleaned_mask[idx_reverse, :])
 
         if center_matrix:
             mean_dm = np.ma.mean(design_matrix, axis=1)
             design_matrix = design_matrix - mean_dm[:, np.newaxis]
+
+        design_matrix = np.ma.array(design_matrix.data*data_unit,
+                                    mask=design_matrix.mask)
         return design_matrix
 
     def reshape_data(self, data_in):

tests/TSO_tests.py
@@ -22,33 +22,51 @@ tso = cascade.TSO.TSOSuite("cascade_test_cpm.ini",
 print(tso.cascade_parameters)
 print(cascade.initialize.cascade_configuration)
 print(tso.cascade_parameters.isInitialized)
+assert tso.cascade_parameters == cascade.initialize.cascade_configuration
+assert tso.cascade_parameters.isInitialized is True
 
 # reset parameters
 tso.execute("reset")
-print(tso.cascade_parameters.isInitialized)
 
-# initialize with providing ini files
-tso.execute("initialize")
 print(tso.cascade_parameters.isInitialized)
+assert tso.cascade_parameters.isInitialized is False
+
+# initialize without providing ini files
+tso.execute("initialize")
+
+print(tso.cascade_parameters.isInitialized)
+assert tso.cascade_parameters.isInitialized is False
 
 # create TSO object and initialize
 tso = cascade.TSO.TSOSuite()
 path = cascade.initialize.default_initialization_path
 tso.execute("initialize", "cascade_test_cpm.ini", "cascade_test_object.ini",
             "cascade_test_data_spectra.ini", path=path)
+
 print(tso.cascade_parameters)
 print(cascade.initialize.cascade_configuration)
 print(tso.cascade_parameters.isInitialized)
+assert tso.cascade_parameters == cascade.initialize.cascade_configuration
+assert tso.cascade_parameters.isInitialized is True
 
 # load data
 tso.execute("load_data")
-plt.imshow(tso.observation.dataset.data[:, :])
-plt.show()
 
-
-plt.imshow(tso.observation.dataset._wavelength)
-plt.show()
-plt.imshow(tso.observation.dataset.wavelength[:, :])
+image0 = tso.observation.dataset.data[:, :].copy()
+image0.set_fill_value(np.nan)
+wave0 = tso.observation.dataset._wavelength
+wave0_min = np.min(wave0)
+wave0_max = np.max(wave0)
+plt.imshow(image0.filled().value,
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto',
+           extent =[0, image0.shape[1], wave0_min, wave0_max])
+plt.colorbar().set_label("Intensity ({})".format(image0.data.unit))
+plt.xlabel("Integration Number")
+plt.ylabel("Wavelength")
+plt.title('Science data')
 plt.show()
 
 with quantity_support():
@@ -64,42 +82,91 @@ with quantity_support():
 print(tso.observation.dataset.time.data.shape)
 print(type(tso.observation.dataset.time.data))
 print(tso.observation.dataset.time.data.unit)
+assert tso.observation.dataset.time.data.shape == (130, 280)
+assert type(tso.observation.dataset.time.data) == u.quantity.Quantity
+assert tso.observation.dataset.time.data.unit == u.dimensionless_unscaled
+
 
 # subtract background
 tso.execute("subtract_background")
-plt.imshow(tso.observation.dataset.data[:, :])
-plt.show()
-
-plt.imshow(tso.observation.dataset.mask)
-plt.show()
 
 with quantity_support():
     plt.plot(tso.observation.dataset.time[80, :],
              tso.observation.dataset.data[80, :])
     plt.show()
+
+print(hasattr(tso.observation.dataset, 'isBackgroundSubtracted'))
+assert hasattr(tso.observation.dataset, 'isBackgroundSubtracted') is False
+
 
 # sigma clip data
 tso.execute("sigma_clip_data")
-plt.imshow(tso.observation.dataset.data[:, :])
+
+image0 = tso.observation.dataset.data[:, :].copy()
+image0.set_fill_value(np.nan)
+wave0 = tso.observation.dataset._wavelength
+wave0_min = np.min(wave0)
+wave0_max = np.max(wave0)
+plt.imshow(image0.filled().value,
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto',
+           extent =[0, image0.shape[1], wave0_min, wave0_max])
+plt.colorbar().set_label("Intensity ({})".format(image0.data.unit))
+plt.xlabel("Integration Number")
+plt.ylabel("Wavelength")
+plt.title('Science data')
 plt.show()
-plt.imshow(tso.observation.dataset.mask[:, :])
+
+plt.imshow(tso.observation.dataset.mask[:, :],
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto',
+           extent =[0, image0.shape[1], wave0_min, wave0_max])
+plt.xlabel("Integration Number")
+plt.ylabel("Wavelength")
+plt.title('Data mask')
 plt.show()
+
+time0 = tso.observation.dataset.time[80, :].copy()
+data0 = tso.observation.dataset.data[80, :].copy()
+time0.set_fill_value(np.nan)
+data0.set_fill_value(np.nan)
 with quantity_support():
-    plt.plot(tso.observation.dataset.time[80, :],
-             tso.observation.dataset.data[80, :])
+    plt.plot(time0.filled(),
+             data0.filled())
     plt.show()
+
+print(tso.observation.dataset.isSigmaCliped)
+assert tso.observation.dataset.isSigmaCliped is True
 
 # eclipse model
 tso.execute("define_eclipse_model")
-plt.imshow(tso.model.light_curve_interpolated[0][:, :])
+
+plt.imshow(tso.model.light_curve_interpolated[0][:, :],
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto',
+           extent =[0, image0.shape[1], wave0_min, wave0_max])
+plt.xlabel("Integration Number")
+plt.ylabel("Wavelength")
+plt.title('Lightcurve model')
 plt.show()
+
 with quantity_support():
     plt.plot(tso.observation.dataset.time.data[80, :],
              tso.model.light_curve_interpolated[0][80, :])
     plt.show()
 
+print(tso.model.transit_timing)
+assert tso.model.transit_timing == [0.4827232723272327, 0.5172767276727672]
+
 # determine position of source from data set
 tso.execute("determine_source_position")
+
 with quantity_support():
     plt.plot(tso.observation.spectral_trace['wavelength_pixel'],
              tso.observation.spectral_trace['positional_pixel'])
@@ -126,36 +193,70 @@ plt.show()
 
 # setup regressors
 tso.execute("select_regressors")
-print(len(tso.cpm.regressor_list))
-print(len(tso.cpm.regressor_list[0]))
-print(len(tso.cpm.regressor_list[0][0]))
-print(tso.cpm.regressor_list[0][0])
-print(tso.cpm.regressor_list[0][0][0])
-print(tso.cpm.regressor_list[0][0][1])
 
+print(hasattr(tso.cpm, 'regressor_list'))
+assert hasattr(tso.cpm, 'regressor_list') is True
 
-# setup of regression matrix
+# setup of regression matrix without clipping
 tso.return_all_design_matrices()
-print(tso.cpm.design_matrix[0][0][0].shape)
-mask = tso.cpm.design_matrix[0][50][0].mask
-plt.imshow(mask)
-plt.show()
-data_masked = tso.cpm.design_matrix[0][50][0]
-plt.imshow(data_masked)
+
+# 1D case:
+#   regressor list index:
+#       first: [# nod]
+#       second: [# data pixel in wavelength direction]
+#       third: [0=pixel coord; 1=list of regressors]
+#       forth: [0=coordinate wave direction; 1=coordinate spatial direction]
+#   design_matrix:
+#       first: [# nod]
+#       second: [# data pixel in wavelength direction]
+#       third: [0]
+
+i_data_point_in_wavelength_direction = 50
+reg_list = \
+    tso.cpm.regressor_list[0][i_data_point_in_wavelength_direction][1][0]
+reg_matrix = tso.cpm.design_matrix[0][i_data_point_in_wavelength_direction][0]
+
+plt.imshow(reg_matrix.mask)
 plt.show()
 
-tso.return_all_design_matrices(clip=True)
-print(tso.cpm.design_matrix[0][0][0].shape)
-mask = tso.cpm.design_matrix[0][50][0].mask
-plt.imshow(mask)
-plt.show()
-data_masked = tso.cpm.design_matrix[0][50][0]
-plt.imshow(data_masked)
+data_masked = reg_matrix
+data_masked.set_fill_value(np.nan)
+plt.imshow(data_masked.filled().value,
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto')
+plt.colorbar().set_label("Intensity ({})".format(image0.data.unit))
+plt.xlabel("Integration Number")
+plt.ylabel("Regressor Number")
+plt.title('Not clipped regressor matrix')
 plt.show()
 
+# setup of regression matrix with clipping
+tso.return_all_design_matrices(clip=True, clip_pctl_time=0.08,
+                               clip_pctl_regressors=0.04)
+
+reg_matrix = tso.cpm.design_matrix[0][i_data_point_in_wavelength_direction][0]
+
+plt.imshow(reg_matrix.mask)
+plt.show()
+
+data_masked = reg_matrix
+data_masked.set_fill_value(np.nan)
+plt.imshow(data_masked.filled().value,
+           origin='lower',
+           cmap='hot',
+           interpolation='nearest',
+           aspect='auto')
+plt.colorbar().set_label("Intensity ({})".format(image0.data.unit))
+plt.xlabel("Integration Number")
+plt.ylabel("Regressor Number")
+plt.title('Clipped regressor matrix')
+plt.show()
 
 # create calibrated time series and derive planetary signal
 tso.execute("calibrate_timeseries")
+
 print(np.ma.max(tso.calibration_results.regularization))
 print(np.ma.min(tso.calibration_results.regularization))
 plt.imshow(tso.calibration_results.regularization)
@@ -213,12 +314,12 @@ ax.set_ylabel('Fp/Fstar')
 ax.set_xlabel('Wavelength')
 plt.show()
 
-for i in range(71):
-    ks, prob = stats.ks_2samp(flux_ian.value,
-                          (tso.exoplanet_spectrum.spectrum.data.data.
-                           value[~tso.exoplanet_spectrum.spectrum.mask])[::-1] -
-                           (0.035-i*0.001))
-    print(-(0.035-i*0.001), ks, prob)
+#for i in range(71):
+#    ks, prob = stats.ks_2samp(flux_ian.value,
+#                          (tso.exoplanet_spectrum.spectrum.data.data.
+#                           value[~tso.exoplanet_spectrum.spectrum.mask])[::-1] -
+#                           (0.035-i*0.001))
+#    print(-(0.035-i*0.001), ks, prob)
 
 
 # save planetary signal
@@ -226,6 +327,30 @@ tso.execute("save_results")
 
 # plot planetary signal
 tso.execute("plot_results")
+
+#########################################################
+
+cal_file = 'IRSX_SL1_S18.18.0_cal_rsrf_default_droop.tbl'
+path_cal = '/home/bouwman/IRS_Calibration/SPITZER_IA_CAL/S18.18.0/'
+rsrf = ascii.read(path_cal+cal_file, data_start=0)
+wave_rsrf = rsrf['col1'].data
+rsrf_1 = rsrf['col2'].data
+rsrf_2 = rsrf['col4'].data
+
+wave = tso.exoplanet_spectrum.spectrum.wavelength.data.value
+star = 0.5 * (wave/wave[0])**-2
+rsrf_new = (tso.calibration_results.parameters[0, :, 0])/(tso.calibration_results.signal[:,0])
+rsrf_old = (rsrf_1 + rsrf_2)*0.47*star
+plt.plot(wave, rsrf_new)
+plt.plot(wave, rsrf_old)
+plt.show()
+
+plt.plot(wave, rsrf_new/rsrf_old)
+plt.show()
+
+
+
+#####################################################
 
 cal_signal_depth = float(tso.cascade_parameters.cpm_calibration_signal_depth)
 mean_eclipse_depth = float(tso.cascade_parameters.observations_median_signal)