efb68394ce75b95bcb9cc5137643a028377d71a0
Jeroen Bouwman <bouwman@mpia.de>
2020-04-23 17:56:14 +0200

    fixed bug in run_cascade.py script

--- scripts/run_cascade.py
+++ scripts/run_cascade.py
@@ -57,20 +57,39 @@ def log(string, color, font="slant", figlet=False):
 @click.option('--save_path', '-sp', nargs=1, type=click.STRING)
 @click.option('--show_plots', '-plt', is_flag=True)
 @click.option('--no_warnings', '-nw', is_flag=True)
-def run_cascade(initfiles, path, data_path, show_plots, no_warnings):
+def run_cascade(initfiles, init_path, data_path, save_path, show_plots,
+                no_warnings):
     """
     Run CASCADe.
+    
+    This function enables the user to run CASCADe from the command line. 
 
     Parameters
     ----------
     initfiles : 'str'
-        Names of the ini files defining data set and cascade behaviour.
-    path : 'str'
-        Path to configuration files.
+        The names of the configuration files used to define the data and 
+        CASCADe behaviour. Can be one or multiple file names.
+    init_path : 'str'
+        Path to the directory containing the configuration files. If not
+        specified, the value set by the environment variable
+        CASCADE_INITIALIZATION_FILE_PATH is used or if neither is set it
+        defaults to the CASCADe default value of the CASCADe distribution.
     data_path : 'str'
-        Path to spectral data files.
+        Path to the data (observations, calibration files, exoplanet catalogs,
+        archive databases) needed by CASCADe to function. If not set, the
+         the value set by the environment variable
+        CASCADE_DATA_PATH is used or if neither is set it
+        defaults to the CASCADe default value of the CASCADe distribution.
+    save_path : 'str'
+        path to the directory where CASCADe saves it's results (plots,
+        extracted planetary spectra). If not specified, the value set by
+        the environment variable CASCADE_SAVE_PATH is used, or if neither
+        is set it defaults to the CASCADe default value of the CASCADe
+        distribution.
     show_plots : 'bool'
-    no_warnings: 'bool'
+        If True plot windows are opened. Default is False.
+    no_warnings : 'bool'
+        If set no warning messages are printed to stdev. Default is True
 
     Returns
     -------
@@ -79,8 +98,8 @@ def run_cascade(initfiles, path, data_path, show_plots, no_warnings):
     """
     if no_warnings:
         os.environ["CASCADE_WARNINGS"] = 'off'
-    if path is not None:
-        os.environ["CASCADE_INITIALIZATION_FILE_PATH"] = path
+    if init_path is not None:
+        os.environ["CASCADE_INITIALIZATION_FILE_PATH"] = init_path
     if data_path is not None:
         os.environ["CASCADE_DATA_PATH"] = data_path
     if save_path is not None:
@@ -95,10 +114,10 @@ def run_cascade(initfiles, path, data_path, show_plots, no_warnings):
         "green")
     log("The data directory is: {}".format(os.environ["CASCADE_DATA_PATH"]),
         "green")
-     log("The save directory is: {}".format(os.environ["CASCADE_SAVE_PATH"]),
+    log("The save directory is: {}".format(os.environ["CASCADE_SAVE_PATH"]),
         "green")
-     log("Note that the speciefied directories will ignored if an absolute "
-         "path is spesified in the initialization files", "green")
+    log("Note that the speciefied directories will ignored if an absolute "
+        "path is spesified in the initialization files", "green")
 
     start_time = time.time()
 
